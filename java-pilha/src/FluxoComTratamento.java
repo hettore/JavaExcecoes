fc3aee7f9758af098680be4e2273fef94d807476
Hettore Eduardo <duh.barretoo@gmail.com>
2023-02-20 20:47:40 -0300

    Lançando exceções

---
+++ java-pilha/src/FluxoComTratamento.java
@@ -0,0 +1,31 @@
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Ini do main");
+        try {
+        	metodo1();
+        } catch(ArithmeticException | NullPointerException e) {
+        	String msg =  e.getMessage();
+        	System.out.println("Exception " + msg);
+        	e.printStackTrace();
+        }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        
+        throw new ArithmeticException("Deu errado!");
+        
+        //System.out.println("Fim do metodo2");
+    }
+	
+}
